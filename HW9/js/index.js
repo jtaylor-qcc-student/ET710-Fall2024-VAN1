3cc17bd290ee6ce3cfde7dcee8cf21e1274f25bf
jtaylor-qcc-student <jaatay2003@gmail.com>
2024-11-21 19:59:07 +0000

    start of hw9

---
+++ HW9/js/index.js
@@ -0,0 +1,47 @@
+function checkDate()
+{
+    const userDate = document.forms["dateForm"]["userDate"].value;
+    const outputMsg = document.getElementById('output');
+
+    let isWeekend = checkIfWeekend(userDate);
+    let isHoliday = checkIfHoliday(userDate);
+
+    if(isWeekend || isHoliday)
+    {
+        outputMsg.textContent = "You can relax today.";
+    }
+    else
+    {
+        outputMsg.textContent = "It's a working day.";
+    }
+}
+
+function checkIfWeekend(today = "")
+{
+    let date = new Date(today);
+    // 0-6
+    let currentDayOfWeek = date.getUTCDay();
+
+    if (currentDayOfWeek == 0 || currentDayOfWeek == 5 || currentDayOfWeek == 6)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+function checkIfHoliday(today = "")
+{
+    let date = new Date(today);
+    let currentDayOfMonth = date.getUTCDate();
+    // Add 1 because the result is zero based.
+    // 0-11
+    let currentMonth = (date.getMonth() + 1);
+
+    if (currentDayOfMonth == 11 && currentMonth == 25) 
+    {
+        return true;
+    }
+
+    return false;
+}
